27c3f91e26c98b03a171812f23650294ba6ef4a3
Sarah <130007719+doctype-html@users.noreply.github.com>
2024-10-13 12:13:00 -0400

    Create 1script.py
    i have a diff repository for my scripts and i just transfer them which is why its alr completed but yeah :3

---
+++ 1script.py
@@ -0,0 +1,38 @@
+import getpass
+import subprocess
+import time
+
+def change_password(new_password):
+    """change password"""
+    username = getpass.getuser()  # get user
+    try:
+        subprocess.run(
+            ['sudo', 'chpasswd'],
+            input=f'{username}:{new_password}'.encode(),
+            check=True
+        )
+        print(f"yay password changed!")
+    except subprocess.CalledProcessError as e:
+        print(f"an unexpected error occured {e}")
+
+passwords = [
+    "V4aiU1zh!Fr",
+    "eech5UdLQL!",
+    "rEI05Y9cKC!",
+    "t9iu!8geKTi",
+    "uOLctmvn!jc"
+]
+
+def rotate_passwords():
+    """rotates thru predetermined set of passwords periodically"""
+    while True:
+        for password in passwords:
+            change_password(password)
+            time.sleep(1800)  # password changes every 30 mins
+if __name__ == "__main__":
+    try: rotate_passwords()
+
+# SOME NOTES that i think you already know but...
+ # 1) run this script with sudo "sudo python3 script1.py" so the code actually works
+ # 2) sudo chpasswd 2 change password
+ # 3) after that it should rotate thru pre-determined list every 30 mins
